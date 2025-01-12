b4bd06a28570d1b50990955475bc1c1ddf2b8643
Nelson-Lamounier <lamounierincolor@gmail.com>
2025-01-12 21:43:44 +0000

    Netlify Deployment strictness settings

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: './src/setupTests.ts', // Optional: Add setup file
+    setupFiles: './srcsetup.ts/setupTests.ts', // Optional: Add setup file
     exclude: [...configDefaults.exclude, 'e2e/*'], // Example custom exclusion
   },
 });
