95f330942d632b7735f5303d160ff2e2d05f98a1
Nelson-Lamounier <lamounierincolor@gmail.com>
2025-01-15 21:21:04 +0000

    Test for deploy

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,11 +1,1 @@
-export { default as CheckboxWithLink} from "./form/CheckBox"
-export { default as InputField} from "./form/InputFields"
-export { default as SelectField} from "./form/SelectFields"
 
-export { default as Logo} from "./logo/Logo"
-export { default as LogoSmall} from "./logo/LogoSm"
-
-
-export { default as Navbar} from "./navbar/Navbar"
-
-export { default as Footer} from "./Footer"
